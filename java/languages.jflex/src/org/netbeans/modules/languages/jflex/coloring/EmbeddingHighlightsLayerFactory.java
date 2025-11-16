6f42780811caf6cdb1250e49e8044ffcafa3f15f
Matthias Bläsing <mblaesing@doppel-helix.eu>
2025-11-16 15:39:12 +0100

    Background Coloring for embedded java sequences

---
+++ java/languages.jflex/src/org/netbeans/modules/languages/jflex/coloring/EmbeddingHighlightsLayerFactory.java
@@ -0,0 +1,42 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.netbeans.modules.languages.jflex.coloring;
+
+import org.netbeans.api.editor.mimelookup.MimeRegistration;
+import org.netbeans.modules.languages.jflex.JflexLanguage;
+import org.netbeans.spi.editor.highlighting.HighlightsLayer;
+import org.netbeans.spi.editor.highlighting.HighlightsLayerFactory;
+import org.netbeans.spi.editor.highlighting.ZOrder;
+
+/**
+ * This class is inspired/adapted from org.netbeans.modules.html.editor.coloring.EmbeddingHighlightsLayerFactory
+ */
+@MimeRegistration(mimeType = JflexLanguage.MIME_TYPE, service = HighlightsLayerFactory.class)
+public class EmbeddingHighlightsLayerFactory implements HighlightsLayerFactory {
+
+    @Override
+    public HighlightsLayer[] createLayers(Context context) {
+        return new HighlightsLayer[]{HighlightsLayer.create("jflex-embedding-highlight-layer",
+                ZOrder.BOTTOM_RACK.forPosition(100),  //we need to have lower priority than the default syntax from options - 0
+                true, 
+                new EmbeddingHighlightsContainer(context.getDocument()))}; //NOI18N
+    }
+
+}
