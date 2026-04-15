6c46e072d1084c82ff1ed6fde81452f0ec832fff
Matthias Bläsing <mblaesing@doppel-helix.eu>
2026-04-15 18:59:06 +0200

    Listen for file changes in project directory based on prefix/suffix of files

--- ide/languages.env/src/org/netbeans/modules/languages/env/project/EnvFileImpl.java
+++ ide/languages.env/src/org/netbeans/modules/languages/env/project/EnvFileImpl.java
@@ -25,10 +25,15 @@ import javax.swing.event.ChangeListener;
 import org.netbeans.api.project.Project;
 import org.netbeans.modules.languages.env.EnvFileResolver;
 import org.netbeans.modules.web.common.spi.ImportantFilesImplementation;
-import org.netbeans.modules.web.common.spi.ImportantFilesSupport;
 import org.netbeans.spi.project.LookupProvider;
 import org.netbeans.spi.project.ProjectServiceProvider;
+import org.openide.filesystems.FileChangeAdapter;
+import org.openide.filesystems.FileChangeListener;
+import org.openide.filesystems.FileEvent;
 import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileRenameEvent;
+import org.openide.util.ChangeSupport;
+import org.openide.util.WeakListeners;
 
 @ProjectServiceProvider(service = ImportantFilesImplementation.class, projectTypes = {
     @LookupProvider.Registration.ProjectType(id = "org-netbeans-modules-web-clientproject"),
@@ -36,12 +41,16 @@ import org.openide.filesystems.FileObject;
 public class EnvFileImpl implements ImportantFilesImplementation {
 
     private final Project project;
-    private final ImportantFilesSupport support;
+    private final FileObject projectDir;
+    private final FilesListener fileChangeListener = new FilesListener();
+    @SuppressWarnings("this-escape")
+    private final ChangeSupport changeSupport = new ChangeSupport(this);
 
     public EnvFileImpl(Project project) {
         assert project != null;
         this.project = project;
-        support = ImportantFilesSupport.create(project.getProjectDirectory(), EnvFileResolver.DOT_ENV);
+        this.projectDir = project.getProjectDirectory();
+        projectDir.addFileChangeListener(WeakListeners.create(FileChangeListener.class, fileChangeListener, projectDir));
     }
 
     @Override
@@ -49,7 +58,9 @@ public class EnvFileImpl implements ImportantFilesImplementation {
         //custom env files
         List<FileInfo> envFiles = new ArrayList<>();
         for (FileObject file : project.getProjectDirectory().getChildren()) {
-            if (!file.isFolder() && (file.getMIMEType().equals(EnvFileResolver.MIME_TYPE))) {
+            if ((file.getNameExt().startsWith(EnvFileResolver.DOT_ENV) || file.getNameExt().endsWith(EnvFileResolver.DOT_ENV))
+                    && !file.isFolder()
+                    && (file.getMIMEType().equals(EnvFileResolver.MIME_TYPE))) {
                 envFiles.add(new FileInfo(file));
             }
         }
@@ -58,12 +69,37 @@ public class EnvFileImpl implements ImportantFilesImplementation {
 
     @Override
     public void addChangeListener(ChangeListener listener) {
-        support.addChangeListener(listener);
+        changeSupport.addChangeListener(listener);
     }
 
     @Override
     public void removeChangeListener(ChangeListener listener) {
-        support.removeChangeListener(listener);
+        changeSupport.removeChangeListener(listener);
     }
 
+    private final class FilesListener extends FileChangeAdapter {
+
+        @Override
+        public void fileRenamed(FileRenameEvent fe) {
+            check(fe.getFile().getNameExt());
+            check(fe.getName() + "." + fe.getExt()); // NOI18N
+        }
+
+        @Override
+        public void fileDeleted(FileEvent fe) {
+            check(fe.getFile().getNameExt());
+        }
+
+        @Override
+        public void fileDataCreated(FileEvent fe) {
+            check(fe.getFile().getNameExt());
+        }
+
+        private void check(String filename) {
+            if (filename.startsWith(EnvFileResolver.DOT_ENV) || filename.endsWith(EnvFileResolver.DOT_ENV)) {
+                changeSupport.fireChange();
+            }
+        }
+
+    }
 }
